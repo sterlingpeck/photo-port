3fca8e476e9c3b3ce4335f455a0cc68df40f362d
Sterling Peck <peck.sterling@gmail.com>
2022-07-22 11:11:01 -0600

    code snapshot

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import PhotoList from "../PhotoList";
-import { capitalizeFirstLetter } from "../../utils/helpers";
+import React from 'react';
+import PhotoList from '../PhotoList';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Gallery(props) {
   const { currentCategory } = props;
